69176773b9eb30cd959dc3ee665d148cd751ff83
erasto-projects <erastusmunyao2022@gmail.com>
2024-10-25 22:19:00 +0300

    Created fronted and backend

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const mysql = require('mysql2');
 require('dotenv').config();
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(cors());
