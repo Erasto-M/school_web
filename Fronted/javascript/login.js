0c0ef7e959b019b9ef831ea27409aaafbe1705d9
Erastus Munyao kyalo(Flutter Dev) <kyalo.erastus21@students.dkut.ac.ke>
2024-10-25 22:44:37 +0300

    Update login.js

--- Fronted/javascript/login.js
+++ Fronted/javascript/login.js
@@ -30,7 +30,7 @@ document.addEventListener('DOMContentLoaded', () => {
   
         // Attempt to submit form data via fetch
         try {
-            const response = await fetch('http://127.0.0.1:3000/login', {
+            const response = await fetch('https://school-web-backend.onrender.com/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
